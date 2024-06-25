c1cc223921246638e887c34714561ee69fe01ad0
Lucas <lucasvieirasouzaa@gmail.com>
2024-06-25 16:34:31 -0300

    Cleanning code

--- styles/misc/dark-mode.js
+++ styles/misc/dark-mode.js
@@ -1,21 +1,22 @@
-// dark-mode.js
-
 document.addEventListener("DOMContentLoaded", function() {
   const toggleSwitch = document.getElementById("toggle-dark-mode");
 
-  // Verifica o estado salvo no Local Storage ao carregar a página
-  if (localStorage.getItem("darkMode") === "enabled") {
+  if (localStorage.getItem("darkMode") === "enabled")
+    {
       document.body.classList.add("dark-mode");
       toggleSwitch.checked = true;
-  }
+    }
 
   toggleSwitch.addEventListener("change", function() {
-      if (toggleSwitch.checked) {
+      if (toggleSwitch.checked) 
+       {
           document.body.classList.add("dark-mode");
           localStorage.setItem("darkMode", "enabled");
-      } else {
+       } 
+      else 
+       {
           document.body.classList.remove("dark-mode");
           localStorage.setItem("darkMode", "disabled");
-      }
+        }
   });
 });
